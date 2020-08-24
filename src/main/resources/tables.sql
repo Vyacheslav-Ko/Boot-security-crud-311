2d4ca9f6c82d5a6a972d664366aaf9e5615fc1dd
Vyacheslav-Ko <Ignition1978@gmail.com>
2020-08-24 10:09:23 +0500

    before SecurityConfig

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,27 +1,26 @@
 
 -- Table: users
--- CREATE TABLE users (
---   id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   username VARCHAR(255) NOT NULL,
---   password VARCHAR(255) NOT NULL,
---   email    VARCHAR(255) NOT NULL
--- );
-
 CREATE TABLE users (
-  `id`          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `username`    VARCHAR(255)    NOT NULL,
-  `password`    VARCHAR(255)    NOT NULL,
-  `email`       VARCHAR(255)    NOT NULL,
---   PRIMARY KEY (`id`),
-  UNIQUE INDEX `user_UNIQUE` (`username` ASC) VISIBLE, --выкидывает exception
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
-  );
+  id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email    VARCHAR(255) NOT NULL
+);
+
+-- CREATE TABLE users (
+--   `id`          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   `username`    VARCHAR(255)    NOT NULL,
+--   `password`    VARCHAR(255)    NOT NULL,
+--   `email`       VARCHAR(255)    NOT NULL,
+-- --   PRIMARY KEY (`id`),
+--   UNIQUE INDEX `user_UNIQUE` (`username` ASC) VISIBLE, --выкидывает exception
+--   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
+--   );
 
 -- Table: roles
 CREATE TABLE roles (
   id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL
-  --добавить UNIQUE ?
 );
 
 
@@ -45,3 +44,4 @@ INSERT INTO roles VALUES (1, 'ROLE_USER');
 INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
 
 INSERT INTO user_roles VALUES (1, 2);
+INSERT INTO user_roles VALUES (1, 1);
